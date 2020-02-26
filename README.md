88b986577f9eb9a89335e2fbbb3e94e8dcbf6803
defilercompiler <krystof.kuzelka@gmail.com>
2020-02-25 22:37:31 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# color-shift
+```
+color-shift
+
+Usage: color-shift --file ARG --shift ARG --src-channel ARG --tgt-channel ARG
+
+Available options:
+  -h,--help                Show this help text
+  --file ARG               Image path
+  --shift ARG              By how many pixels to shift the source channel
+  --src-channel ARG        Source Channel - red | green | blue
+  --tgt-channel ARG        Target Channel - red | green | blue
+```
+
+## Examples
+![Sample](resources/1.jpg)
+
---
+++ src/Main.hs
@@ -0,0 +1,152 @@
+{-# LANGUAGE BangPatterns     #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE RecordWildCards  #-}
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+module Main where
+
+import           Codec.Picture
+import           Codec.Picture.Types
+import           Control.Lens
+import           Control.Monad
+import           Control.Monad.Primitive
+import           Control.Monad.ST
+import           Control.Monad.ST
+import           Data.List.Split
+import           Data.Maybe
+import qualified Data.Sequence           as Seq
+import           Data.Tuple.Select
+import qualified Data.Vector.Storable    as VS
+import           Data.Word               (Word8)
+import           Options.Applicative
+import           System.FilePath.Lens
+
+
+main :: IO ()
+main = do
+  cli@CLI {..} <- either (error . show) id . validateCLI <$> execParser cliParser
+  src <- either (error "Couldn't read image") convertRGBA8 <$> readImage cImgPath
+  let new = makeImage cShift ch1 ch2 src
+      ch1 = txtToChannel cChannelSrc
+      ch2 = txtToChannel cChannelTgt
+  writePng (makeFileName cli cImgPath) new
+  where
+    makeFileName CLI {..} imgPath =
+      let baseDir     = imgPath ^. directory
+          [name, ext] = case splitOn "." $ imgPath ^. filename of
+            (n:x:_) -> [n, x]
+            _       -> error "Invalid filename/extension."
+      in baseDir <> "/" <> name <> "-shifted-" <> cChannelSrc
+          <> "-" <> cChannelTgt <> "-"
+          <> show cShift <> "." <> ext
+
+    validateCLI cli@(CLI _ cSh cSrc cTgt)
+      | not $ elem cSrc ["red", "green", "blue"] = Left "Source channel not in red | green | blue"
+      | not $ elem cTgt ["red", "green", "blue"] = Left "Target channel not in red | green | blue"
+      | otherwise = Right cli
+
+    cliParser = info (helper <*> parseCLI) (header "color-shift")
+
+    txtToChannel ch = case ch of
+                  "red"   -> Red
+                  "blue"  -> Blue
+                  "green" -> Green
+
+    parseCLI = CLI
+      <$> strOption (long "file" <> help "Image path")
+      <*> option auto (long "shift" <> help "By how many pixels to shift the source channel")
+      <*> strOption (long "src-channel" <> help "Source Channel - red | green | blue")
+      <*> strOption (long "tgt-channel" <> help "Target Channel - red | green | blue")
+
+
+-- | CLI.
+data CLI = CLI
+  { cImgPath    :: FilePath -- ^ Path to the file to process.
+  , cShift      :: Int -- ^ By which amount to shift
+  , cChannelSrc :: String -- ^ Source channel
+  , cChannelTgt :: String -- ^ Target channel
+  } deriving (Eq, Show)
+
+makeImage
+  :: Int -- ^ By how much to shift
+  -> Channel
+  -> Channel
+  -> Image PixelRGBA8 -- ^ Source image.
+  -> Image PixelRGBA8
+makeImage
+  = shiftChannel
+
+-- | Indexed from (0 0) - top left to (imgWidth-1 imgHeight-1) - bottom right
+data Rectangle = Rectangle
+  { x1 :: Int
+  , y1 :: Int
+  , x2 :: Int
+  , y2 :: Int
+} deriving (Show)
+
+-- |
+shiftChannel :: Int -> Channel -> Channel -> Image PixelRGBA8 -> Image PixelRGBA8
+shiftChannel a ch1 ch2 src@Image {..} = runST $ do
+  mimg <- unsafeThawImage src
+  void $ writeRectangle mimg np (Rectangle 0 0 (imageWidth-1) (imageHeight-1))
+  unsafeFreezeImage mimg
+  where np = zipChannels a ch1 ch2 imageData
+
+zipChannels :: Int -> Channel -> Channel -> VS.Vector Word8 -> Seq.Seq PixelRGBA8
+zipChannels a ch1 ch2 id =
+  case (ch1, ch2) of
+    (Red, Red)     -> fmap makePixel (Seq.zip4 (sh r) g b al)
+    (Red, Green)   -> fmap makePixel (Seq.zip4 r (sh r) b al)
+    (Red, Blue)    -> fmap makePixel (Seq.zip4 r g (sh r) al)
+    (Green, Green) -> fmap makePixel (Seq.zip4 r (sh g) b al)
+    (Green, Red)   -> fmap makePixel (Seq.zip4 (sh g) g b al)
+    (Green, Blue)  -> fmap makePixel (Seq.zip4 r g (sh g) al)
+    (Blue, Blue)   -> fmap makePixel (Seq.zip4 r g (sh b) al)
+    (Blue, Red)    -> fmap makePixel (Seq.zip4 (sh b) g b al)
+    (Blue, Green)  -> fmap makePixel (Seq.zip4 r (sh b) b al)
+  where makePixel d = PixelRGBA8 (sel1 d) (sel2 d) (sel3 d) (sel4 d)
+        r = extractRed id
+        g = extractGreen id
+        b = extractBlue id
+        al = extractAlpha id
+        sh sq = (Seq.drop a sq) Seq.>< (Seq.take a sq)
+
+-- | Which channel are we shifting
+data Channel = Red | Green | Blue
+
+extractRed :: VS.Vector Word8 -> Seq.Seq Word8
+extractRed = extractChannel 0
+
+extractGreen :: VS.Vector Word8 -> Seq.Seq Word8
+extractGreen = extractChannel 1
+
+extractBlue :: VS.Vector Word8 -> Seq.Seq Word8
+extractBlue = extractChannel 2
+
+extractAlpha :: VS.Vector Word8 -> Seq.Seq Word8
+extractAlpha = extractChannel 3
+
+extractChannel :: Int -> VS.Vector Word8 -> Seq.Seq Word8
+extractChannel = go Seq.empty
+  where
+    go !acc !a !d
+      | VS.length d == 0    = acc
+      | otherwise =
+        go (acc Seq.|> makePixel a (VS.take 4 d)) a (VS.drop 4 d)
+
+    makePixel a d = d VS.! a
+
+
+-- | Write vc vector of pixels of Rectangle rt dimensions into mutable img mmg
+writeRectangle :: (Control.Monad.Primitive.PrimMonad m, Pixel a)
+  => MutableImage (Control.Monad.Primitive.PrimState m) a -> Seq.Seq a -> Rectangle -> m (Image a)
+writeRectangle mmg vc rt =
+  writeRow (x1 rt) (x2 rt) (x1 rt) (y1 rt) (y2 rt) vc mmg
+  where
+    writeRow c c' ic r r' v mimg
+        | Seq.length v == 0 = unsafeFreezeImage mimg
+        | c > c' = writeRow ic c' ic (r + 1) r' (Seq.drop (c'-ic+1) v) mimg
+        | otherwise = do
+            writePixel mimg c r (Seq.index v (c - ic))
+            writeRow (c + 1) c' ic r r' v mimg
+
